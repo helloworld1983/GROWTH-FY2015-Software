87d27113246f2944e5d1b499e4ab11b64e787270
Takayuki Yuasa <yuasatakayuki@gmail.com>
2015-12-07 14:38:01 +0900

    install_apt-get.sh - apt-get commands prepared for fy2015a

---
+++ raspi_setup/install_apt-get.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y zsh
+sudo apt-get install -y fswebcam
+sudo apt-get install -y git dpkg-dev make g++ gcc binutils
+sudo apt-get install -y libx11-dev libxpm-dev libxft-dev libxext-dev python-dev
+sudo apt-get install -y ruby ruby-dev rails wget curl curl-dev zsh
+sudo apt-get install -y build-essential curl m4 texinfo libbz2-dev
+sudo apt-get install -y libcurl4-openssl-dev libexpat-dev libncurses-dev
+sudo apt-get install -y zlib1g-dev chromium libx11-dev
+sudo apt-get install -y imagemagick libjpeg8-dev
+sudo apt-get install -y git cmake swig subversion
+sudo apt-get install -y libboost1.50-all libxerces-c-dev
+sudo apt-get install -y wicd-curses
