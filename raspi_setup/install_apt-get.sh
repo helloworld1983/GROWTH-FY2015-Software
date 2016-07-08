98d95b47e0a4bf1f891abc8f1c0dc50599cd0221
Takayuki Yuasa <yuasatakayuki@gmail.com>
2016-07-09 00:50:13 +0900

    WIP - yaml-cpp, cfitsio, zmq, supervisor were added

--- raspi_setup/install_apt-get.sh
+++ raspi_setup/install_apt-get.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y zsh
 sudo apt-get install -y fswebcam
-sudo apt-get install -y git dpkg-dev make g++ gcc binutils
+sudo apt-get install -y git dpkg-dev make binutils
 sudo apt-get install -y libx11-dev libxpm-dev libxft-dev libxext-dev python-dev
 sudo apt-get install -y ruby ruby-dev rails wget curl curl-dev zsh
 sudo apt-get install -y build-essential curl m4 texinfo libbz2-dev
@@ -12,4 +12,5 @@ sudo apt-get install -y zlib1g-dev chromium libx11-dev
 sudo apt-get install -y imagemagick libjpeg8-dev
 sudo apt-get install -y git cmake swig subversion
 sudo apt-get install -y libboost1.50-all libxerces-c-dev
-sudo apt-get install -y wicd-curses
+sudo apt-get install -y wicd-curses libzmq-dev python-zmq libcfitsio3-dev libyaml-cpp-dev
+sudo apt-get install -y supervisor
